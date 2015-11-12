f9c1ec41605fee7424559d7882d4c752da339786
Jackson Souza <jacksongba.10@gmail.com>
2015-11-12 16:53:08 -0200

    Primeira parte.

---
+++ Memoria/src/memoria/BlocoMemoria.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoria;
+
+
+public class BlocoMemoria {
+    private Memoria[] memoria;
+    private int qtd;
+    public void criar (int tam){
+        memoria = new Memoria[tam]; 
+        qtd = tam;
+    }
+    public void limpar (){
+        for (int i = 0; i < qtd; i++){
+                memoria[i].setValor( 0);
+        }
+    }
+    public void escrever (int inicio, int fim, int conteudo){
+        for (int i = inicio; i <= fim; i++){
+                memoria[i].setValor(conteudo);
+        }
+    }
+    public void liberarMemoria (){
+        for (int i = 0; i < qtd; i++){
+            memoria[i] = null;
+        }
+    }
+
+}
---
+++ Memoria/src/memoria/Memoria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memoria;
+
+/**
+ *
+ * @author JacksonHome
+ */
+public class Memoria {
+    private int valor;
+    
+    public int getValor(){
+        return this.valor;
+    }
+    public void setValor (int valor){
+        this.valor = valor;
+    }
+}
