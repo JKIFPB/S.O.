6a8d6e9d29ad3f549b5e4a31c2b6874e7243272b
gabrieljoyki <gabriel.joyki@gmail.com>
2015-11-23 16:39:04 -0300

    Update Processo.java

--- Memoria/src/memoria/Processo.java
+++ Memoria/src/memoria/Processo.java
@@ -1,23 +1,24 @@
+package memoria;
+
 public class Processo{
-	private int tamanho,base,limite,id;
-	public Processo(int tamanho,int base, int limite, int id){
-		this.tamanho	= tamanho;
-		this.base	= base;
-		this.limite	= limite;
-		this.id		= id;
-	}
-	public int getBase (){
-		return this.base;
-	}
-	public int getLimite(){
-		return this.limite;
-	}
-	public int getId(){
-		return this.id;
-	}
-	public int getTamanho(){
-		return this.Tamanho;
-	}
-
-
+    private int tamanho,base,limite,id;
+    public Processo(int tamanho,int base, int limite, int id){
+        this.tamanho	= tamanho;
+        this.base	= base;
+        this.limite	= limite;
+        this.id		= id;
+    }
+    public int getBase (){
+            return this.base;
+    }
+    public int getLimite(){
+            return this.limite;
+    }
+    public int getId(){
+            return this.id;
+    }
+    public int getTamanho(){
+            return this.tamanho;
+    }
 }
+
