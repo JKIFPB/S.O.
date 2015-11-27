e0d1d06d05865a3cd01253e393e9c8e6c9826477
JKIFPB <jacksongba.10@gmail.com>
2015-11-26 22:26:48 -0300

    Update Area.java

--- Memoria/src/memoria/Area.java
+++ Memoria/src/memoria/Area.java
@@ -3,4 +3,10 @@ package memoria;
 public class Area {
     private int inicio;
     private int fim;
+    public int getInicio(){
+        return this.inicio;
+    }
+    public int getFim(){
+        return this.fim;
+    }
 }
